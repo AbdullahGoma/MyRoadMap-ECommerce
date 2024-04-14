4636a1367bf1916c36f3c9d87fd647a17b325660
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-04-14 22:33:48 +0200

    Diplay All categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _context.Categories.ToList().OrderBy(u => u.DisplayOrder);
+
+            return View(objCategoryList);
         }
     }
 }
