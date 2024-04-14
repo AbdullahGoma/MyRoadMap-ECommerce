d13bd7e82d3bb919a0cc3afb5b22ff77eebe649c
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-04-14 22:17:18 +0200

    Seed Category Table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -14,6 +14,13 @@ namespace BulkyWeb.Data
             // Apply Configurations from all IEntityTypeConfiguration<T>
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+            // Seed Category Data 
+            builder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
             var cascadeFKs = builder.Model.GetEntityTypes()
                                           .SelectMany(t => t.GetForeignKeys())
                                           .Where(fk => fk.DeleteBehavior == DeleteBehavior.Cascade && !fk.IsOwnership);
